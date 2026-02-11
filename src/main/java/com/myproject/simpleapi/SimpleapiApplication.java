12d3f04264b7784669f16e67d61f206af2f88a1c
Radostin Ivanov <radostin1992@abv.bg>
2026-02-11 23:27:19 +0200

    Adding readme to cleanup comments

--- src/main/java/com/myproject/simpleapi/SimpleapiApplication.java
+++ src/main/java/com/myproject/simpleapi/SimpleapiApplication.java
@@ -11,32 +11,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 public class SimpleapiApplication {
 
-	/*
-	 * Basic info:
-	 * - run the application (local installation for development) - ./mvnw
-	 * spring-boot:run
-	 * - run tests - .\mvnw.cmd test
-	 * - rebuild app - ./mvnw clean install
-	 * - curl from terminal - curl http://localhost:8080
-	 * 
-	 * Development with docker:
-	 * - to start the application in a docker container with hot reload - 
-	 * 		docker compose -f docker-compose.dev.yml up
-	 * - to stop the application in a docker container 
-	 * 		docker compose -f docker-compose.dev.yml down
-	 * 
-	 * To test "production" (docker container) that is used in render.com:
-	 * - docker build -t myproject/simpleapi-prod .
-	 * - docker run -p 8080:8080 myproject/simpleapi-prod
-	 * 
-	 * todo and ideas:
-	 * - secure the spring endpoints
-	 * - upgrade java version ?
-	 * - add database and migrator application (flyway)
-	 * - add more endpoints (e.g. POST, PUT, DELETE)
-	 * - add more complex logic to the endpoints (e.g. call external API, process
-	 * data, etc.)
-	 */
 	public static void main(String[] args) {
 		Logger logger = LoggerFactory.getLogger(SimpleapiController.class);
 
